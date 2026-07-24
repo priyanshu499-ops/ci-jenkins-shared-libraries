660b6227001438143690347f2fdd1193c43c748b
Priyanshu Yadav <priyanshu.yadav@opstree.com>
2026-07-24 11:33:29 +0530

    Add nodejs ci/cd files

--- jenkins_seedjob/CD/cd.groovy
+++ jenkins_seedjob/CD/cd.groovy
@@ -1,3 +1,19 @@
+// Common Parameters
+def commonParameters = [
+    [
+        type: 'string',
+        name: 'image_tag',
+        defaultValue: 'latest',
+        description: 'Docker image tag to deploy'
+    ],
+    [
+        type: 'choice',
+        name: 'ENVIRONMENT',
+        choices: ['dev', 'uat', 'prod'],
+        description: 'Select deployment environment'
+    ]
+]
+
 def cdJobs = [
 
     'sdk-instacard-frontend': [
@@ -6,98 +22,100 @@ def cdJobs = [
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CD/sdk-instacard-frontend/Jenkinsfile',
         owner        : 'priyanshu499-ops',
-        logRotatorNum: 5, // keep only last 5 builds' history
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'image_tag', defaultValue: 'latest', description: 'Docker image tag to deploy']
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'design-ui-framework': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CD/design-ui-framework/Jenkinsfile',
         owner        : 'priyanshu499-ops',
-        logRotatorNum: 5, // keep only last 5 builds' history
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'image_tag', defaultValue: 'latest', description: 'Docker image tag to deploy']
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'virtualcard-service': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CD/virtualcard-service/Jenkinsfile',
         owner        : 'priyanshu499-ops',
-        logRotatorNum: 5, // keep only last 5 builds' history
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'image_tag', defaultValue: 'latest', description: 'Docker image tag to deploy']
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'instacard-user-service': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CD/instacard-user-service/Jenkinsfile',
         owner        : 'priyanshu499-ops',
-        logRotatorNum: 5, // keep only last 5 builds' history
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'image_tag', defaultValue: 'latest', description: 'Docker image tag to deploy']
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'instacard-mock-apis': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CD/instacard-mock-apis/Jenkinsfile',
         owner        : 'priyanshu499-ops',
-        logRotatorNum: 5, // keep only last 5 builds' history
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'image_tag', defaultValue: 'latest', description: 'Docker image tag to deploy']
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'auth-service': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CD/auth-service/Jenkinsfile',
         owner        : 'priyanshu499-ops',
-        logRotatorNum: 5, // keep only last 5 builds' history
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'image_tag', defaultValue: 'latest', description: 'Docker image tag to deploy']
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
-     'apigateway': [
+
+    'apigateway': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CD/apigateway/Jenkinsfile',
         owner        : 'priyanshu499-ops',
-        logRotatorNum: 5, // keep only last 5 builds' history
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'image_tag', defaultValue: 'latest', description: 'Docker image tag to deploy']
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
 ]
 
 cdJobs.each { jobName, config ->
+
     pipelineJob("CD/${jobName}") {
-        displayName("${jobName}")
+
+        displayName(jobName)
         description("CD pipeline for ${jobName} | Owner: ${config.owner}")
+
         logRotator {
             numToKeep(config.logRotatorNum)
         }
+
         parameters {
             config.parameters.each { param ->
-                stringParam(param.name, param.defaultValue, param.description)
+                switch (param.type) {
+
+                    case 'choice':
+                        choiceParam(param.name, param.choices, param.description)
+                        break
+
+                    case 'string':
+                        stringParam(param.name, param.defaultValue, param.description)
+                        break
+
+                    default:
+                        stringParam(param.name, param.defaultValue ?: '', param.description)
+                }
             }
         }
+
         definition {
             cpsScm {
                 scm {
--- jenkins_seedjob/CI/ci.groovy
+++ jenkins_seedjob/CI/ci.groovy
@@ -1,3 +1,26 @@
+// Common Parameters
+def commonParameters = [
+    [
+        type: 'choice',
+        name: 'BRANCH',
+        choices: ['dev', 'uat', 'main'],
+        description: 'Select branch to build'
+    ],
+    [
+        type: 'choice',
+        name: 'ENVIRONMENT',
+        choices: ['dev', 'uat', 'prod'],
+        description: 'Select deployment environment'
+    ],
+    [
+        type: 'boolean',
+        name: 'TRIGGER_CD',
+        defaultValue: true,
+        description: 'Trigger CD pipeline after successful CI'
+    ]
+]
+
+// CI Jobs
 def ciJobs = [
     'auth-service': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
@@ -5,130 +28,113 @@ def ciJobs = [
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/auth-service/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'instacard-mock-apis': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/instacard-mock-apis/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'instacard-user-service': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/instacard-user-service/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'virtualcard-service': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/virtualcard-service/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'sdk-instacard-frontend': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/sdk-instacard-frontend/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-       ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'design-ui-framework': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/design-ui-framework/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'apigateway': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/apigateway/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ],
+
     'montra-bom': [
         url          : 'https://github.com/priyanshu499-ops/ci-jenkins-shared-libraries.git',
         credentials  : 'github-token',
         branch       : 'main',
         scriptPath   : 'jenkins_wrapper/CI/montra-bom/Jenkinsfile',
         owner        : 'CI-CD Team',
-        logRotatorNum:  5,
-        parameters   : [
-            [name: 'BRANCH', defaultValue: 'main', description: 'Branch to build'],
-            [name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment'],
-            [type: 'boolean', name: 'TRIGGER_CD', defaultValue: true, description: 'Trigger CD pipeline after successful CI (Yes/No)'],
-            
-        ]
+        logRotatorNum: 5,
+        parameters   : commonParameters
     ]
 ]
 
+// Generate Pipeline Jobs
 ciJobs.each { jobName, config ->
+
     pipelineJob("CI/${jobName}") {
-        displayName("${jobName}")
+
+        displayName(jobName)
+
         description("CI pipeline for ${jobName} | Owner: ${config.owner}")
+
         logRotator {
             numToKeep(config.logRotatorNum)
         }
+
         parameters {
             config.parameters.each { param ->
-                if (param.type == 'boolean') {
-                    booleanParam(param.name, param.defaultValue, param.description)
-                } else {
-                    stringParam(param.name, param.defaultValue, param.description)
+
+                switch (param.type) {
+
+                    case 'choice':
+                        choiceParam(param.name, param.choices, param.description)
+                        break
+
+                    case 'boolean':
+                        booleanParam(param.name, param.defaultValue, param.description)
+                        break
+
+                    default:
+                        stringParam(param.name, param.defaultValue, param.description)
                 }
             }
         }
+
         definition {
             cpsScm {
                 scm {
